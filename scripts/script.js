333f9db6e182aec72805a41ecfe9fe82552b9eb8
brickaa <becca.aaronson@Gmail.com>
2014-08-11 20:21:33 -0500

    add jquery, load google spreadsheet as json

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 /* Main script file */
+
+$(document).ready(function() {
+
+  var data = [];
+
+  //Load JSON data from Google Spreadsheet
+  $.getJSON('https://spreadsheets.google.com/feeds/list/1Vdt46LVbwiivYQw3qNq2Qn7qZktYEIEztKF9z5Ma3Vc/od6/public/values?alt=json', function(json) {
+  	data = json;
+  }).done( function() {
+  	console.log(data);
+  })
+
+});
